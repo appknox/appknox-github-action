ed8fa3da07f2da049566e0b67ad9b9b80f6267ed
Yashvi Agrawal <74981992+yashviagrawal@users.noreply.github.com>
2024-09-16 12:26:54 +0530

    Update

--- src/appknox-inputs.ts
+++ src/appknox-inputs.ts
@@ -1,9 +1,24 @@
 import {RiskThresholdOptions, SarifOptions} from './constants';
 
 export interface AppknoxInputs {
+  /**
+   * Pesonal Access token on Appknox
+   */
   appknoxAccessToken: string;
+
+  /**
+   * Path to the mobile application binary (apk/ipa)
+   */
   filePath: string;
+
+  /**
+   * Minimum risk to fail CI
+   */
   riskThreshold: RiskThresholdOptions;
+
+  /**
+   * Enable SARIF format
+   */
   sarif: SarifOptions;
   region?: string;  // Optional Region parameter
 }
